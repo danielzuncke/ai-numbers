197b086afbf9f519ea9ac92f2cdd5f758e59f461
Xadoom <daniel.zuncke@gmail.com>
2019-05-11 01:35:44 +0200

    beginning to create nn

--- first.py
+++ first.py
@@ -2,10 +2,73 @@
 # numpy
 # jupyter
 # matplotlib
-
-# %% imports
-import numpy as np
+# scipy
 
 # %%
-a = np.array([1, 2, 3])
-print(a)
+# imports and stuff
+import numpy as np
+import scipy.special as sp
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# %%
+# neural network class definition
+class neuralNetwork:
+    # init neural network (nn)
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # create weight matrizes; wih input to hidden, who hidden to output;
+        # w_i_j with node i connecting to node j in next layer;
+        # w11 w21
+        # w12 w22 ..
+        self.wih = np.random.normal(
+            0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = np.random.normal(
+            0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
+        # set learning rate
+        self.lr = learningrate
+
+        # activation function (sigmoind function)
+        self.activation_function = lambda x: sp.expit(x)
+
+        ...
+
+    # train nn
+    def train():
+        ...
+
+    # query nn
+    def query(self, inputs_list):
+        # input list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
+
+        # calculate signals to hidden layer
+        hidden_inputs = np.dot(self.wih, inputs)
+
+        # calculate signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals to final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+
+        # calculate signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+
+# %%
+# create instance of nn
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+learning_rate = 0.3
+
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+n.query([1.0, 0.5, -1.5])
